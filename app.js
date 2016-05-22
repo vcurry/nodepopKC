432807ec313bc7e3c819b13e801a094378989fea
vcurry <verocordobes@hotmail.com>
2016-05-22 21:35:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,52 +2,62 @@
 
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var i18n = require('i18n-2');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var app = express();
 
-/* jshint ignore:start */
-var db = require('./lib/connectMongoose');
-/* jshint ignore:end */
+//requerimos el módulo connectMongoose
+require('./lib/connectMongoose');
 
-// Cargamos las definiciones de todos nuestros modelos
+//Modelos
 require('./models/Anuncio');
 require('./models/Usuario');
 require('./models/PushToken');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));*/
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// registrar lenguajes
-var lang = require('./lib/lang');
-lang.registerLang('en'); // el primero es el idioma por defecto
-lang.registerLang('es');
+i18n.expressBind(app, {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+    cookieName: 'locale'
+});
 
-// poner lenguaje en request leyendo cabecera x-lang
-app.use((req, res, next)=> {
-    req.lang = req.get('x-lang');
+app.use(function(req, res, next) {
+    if(req.query.lang){
+      req.i18n.setLocaleFromQuery();
+  //  res.cookie(config.locale.cookie, req.i18n.getLocale());
+    } else {
+      req.i18n.setLocaleFromQuery();
+      req.i18n.setLocaleFromCookie();
+    }
+  
     next();
 });
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname + '/public')));
 
-// API v1
-app.use('/apiv1/anuncios', require('./routes/apiv1/anuncios'));
-app.use('/apiv1/usuarios', require('./routes/apiv1/usuarios'));
-app.use('/apiv1/pushTokens', require('./routes/apiv1/pushTokens'));
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+//rutas del api
+app.use('/api/v1/anuncios', require('./routes/api/v1/anuncios'));
+app.use('/api/v1/usuarios', require('./routes/api/v1/usuarios'));
+app.use('/api/v1/pushTokens', require('./routes/api/v1/pushTokens'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -61,43 +71,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    /*jshint unused: false*/
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        if (req.path.match(/\/apiv\d+/)) {
-            // llamada de API, devuelvo JSON
-            return res.json({
-                ok: false,
-                error: {code: err.status || 500, message: err.message, err: err}
-            });
-        }
-
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
     });
-    /*jshint unused: true*/
 }
 
 // production error handler
 // no stacktraces leaked to user
-/*jshint unused: false*/
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    if (req.path.match(/\/apiv\d+/)) {
-        // llamada de API, devuelvo JSON
-        return res.json({
-            ok: false,
-            error: {code: err.status || 500, message: err.message, err: err}
-        });
-    }
-
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: {}
     });
 });
-/*jshint unused: true*/
 
 module.exports = app;
